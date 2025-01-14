4e61e5a641ef7c86ff724eb42e6db32bd690f2a7
afzal232 <afzal232@gmail.com>
2025-01-14 09:38:58 +0530

    Recent Counter Impl - Queue

---
+++ src/main/java/org/example/ds/leetcode/queue/problem933/RecentCounter.java
@@ -0,0 +1,27 @@
+package org.example.ds.leetcode.queue.problem933;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Using Queue
+class RecentCounter {
+
+    private Queue<Integer> calls;
+    public RecentCounter() {
+        calls = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        calls.offer(t); // Add the timestamp to the queue
+        while (calls.peek() < t - 3000){ // check at the head of the queue but doesn't remove it yet
+            calls.poll(); // remove from queue
+        }
+        return calls.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
---
+++ src/main/java/org/example/ds/leetcode/queue/problem933/RecentCounterUsingArray.java
@@ -0,0 +1,32 @@
+package org.example.ds.leetcode.queue.problem933;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Using Queue
+class RecentCounterUsingArray {
+
+    private ArrayList<Integer> calls;
+    public RecentCounterUsingArray() {
+        calls = new ArrayList<>();
+    }
+
+    public int ping(int t) {
+        int count = 0;
+        calls.add(t);
+        int start = t-3000;
+        for(int time : calls){
+            if( time >= start && time <= t){
+                count ++;
+            }
+        }
+        return count;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
