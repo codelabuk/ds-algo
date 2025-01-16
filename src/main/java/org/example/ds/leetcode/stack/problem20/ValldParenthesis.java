aa02767bbab71ba7cc9b4cac3ca79808c6d6633a
afzal232 <afzal232@gmail.com>
2025-01-16 16:57:48 +0530

    Add stack impl - Valid Parenthesis

---
+++ src/main/java/org/example/ds/leetcode/stack/problem20/ValldParenthesis.java
@@ -0,0 +1,55 @@
+package org.example.ds.leetcode.stack.problem20;
+
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "()"
+ *
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: s = "()[]{}"
+ *
+ * Output: true
+ *
+ * Example 3:
+ *
+ * Input: s = "(]"
+ *
+ * Output: false
+ *
+ * Example 4:
+ *
+ * Input: s = "([])"
+ *
+ * Output: true
+ */
+public class ValldParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> valStack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c== '(' || c == '{' || c == '['){
+                valStack.push(c);
+            } else {
+                if(valStack.isEmpty()) return false;
+                char top = valStack.pop(); //
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+        return  valStack.isEmpty();
+    }
+}
