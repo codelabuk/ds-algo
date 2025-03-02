58f6b4280d189e5efbd34f362267984f3a823725
afzal232 <afzal232@gmail.com>
2025-03-02 16:47:07 +0530

    ThreadPoolExecutors examples

--- src/main/java/org/example/threading/CreateExecutorServiceExample.java
+++ src/main/java/org/example/threading/CreateExecutorServiceExample.java
@@ -1,13 +1,12 @@
 package org.example.threading;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class CreateExecutorServiceExample {
 
     public static void main(String[] args) {
         ExecutorService singleThreadedExecutor = Executors.newSingleThreadExecutor();
-        for(int i = 0; i <=5; i++) {
+        for (int i = 0; i <= 5; i++) {
             final int taskId = i;
             singleThreadedExecutor.execute(() -> {
                 System.out.println("Single threaded task " + taskId + " executed by : " +
@@ -18,7 +17,7 @@ public class CreateExecutorServiceExample {
 
 
         ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
-        for(int i = 0; i <=5; i++) {
+        for (int i = 0; i <= 5; i++) {
             final int taskId = i;
             fixedThreadPool.execute(() -> {
                 System.out.println("Fixed threaded task " + taskId + " executed by : " +
@@ -28,7 +27,7 @@ public class CreateExecutorServiceExample {
         fixedThreadPool.shutdown();
 
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-        for(int i = 0; i <=5; i++) {
+        for (int i = 0; i <= 5; i++) {
             final int taskId = i;
             cachedThreadPool.execute(() -> {
                 System.out.println("Cached threaded task " + taskId + " executed by : " +
@@ -36,5 +35,18 @@ public class CreateExecutorServiceExample {
             });
         }
         cachedThreadPool.shutdown();
+
+
+        ExecutorService threadPool = new ThreadPoolExecutor(1, 5, 0l,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(2));
+        for (int i = 0; i <= 5; i++) {
+            final int taskId = i;
+            threadPool.execute(() -> {
+                System.out.println("Thread Pooled task " + taskId + " executed by : " +
+                        Thread.currentThread().getName());
+            });
+        }
+        threadPool.shutdown();
+
     }
 }
