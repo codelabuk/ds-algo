9887c0d6b255554ca5e7cb9f7fc5b61a24aa0579
afzal232 <afzal232@gmail.com>
2025-04-12 11:50:30 +0530

    Add practice -dsa module

---
+++ dsa-practice/src/main/java/org/example1/practice/twopointer/ConstantWIndowSum.java
@@ -0,0 +1,34 @@
+package org.example1.practice.twopointer;
+
+public class ConstantWIndowSum {
+
+    private static int solution(int[] arr, int k) {
+        int l = 0, r = k - 1;
+        int n = arr.length;
+        int sum = 0;
+        int maxSum = 0;
+
+        for (int i = l; i <= r; i++) {
+            sum = sum + arr[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        while (r < n-1) {
+            sum = sum - arr[l];
+            l++;
+            r++;
+            sum = sum + arr[r];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-1, 2, 3, 3, 4, 5, -1};
+        System.out.println(solution(arr, 4));
+
+        int[] arr1 = new int[]{8, 2, 3, 3, 4, 5, 21};
+        System.out.println(solution(arr1, 4));
+
+    }
+}
