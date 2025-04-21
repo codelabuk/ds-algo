9262289e7017d4c35c912de81808000f2acc8b24
afzal232 <afzal232@gmail.com>
2025-04-21 23:57:52 +0530

    Rotate Array by d index

--- dsa-practice/src/main/java/org/example1/practice/array/RotateArrayByDIndex.java
+++ dsa-practice/src/main/java/org/example1/practice/array/RotateArrayByDIndex.java
@@ -1,15 +1,19 @@
 package org.example1.practice.array;
 
+import java.util.Arrays;
+
 public class RotateArrayByDIndex {
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 3, 4, 2, 7, 8, 9, 6};
 
+        int[] arr1 = new int[]{1, 3, 4, 2, 7, 8, 9, 6};
+
         leftRotateByD(arr, arr.length, 4);
 
-        for (int j = 0; j < arr.length; j++) {
-            System.out.print(arr[j] + ",");
-        }
+        System.out.println(Arrays.toString(arr));
+        leftRotateByDOptimal(arr1, arr1.length, 4);
+        System.out.println(Arrays.toString(arr1));
     }
 
     private static void leftRotateByD(int[] arr, int n, int d) {
@@ -27,4 +31,28 @@ public class RotateArrayByDIndex {
         }
 
     }
+
+    /**
+     *
+     * @param arr
+     * @param n
+     * @param d
+     */
+    private static void leftRotateByDOptimal(int[] arr, int n, int d) {
+        reverse(arr, 0, n - d - 1);
+        reverse(arr, n - d, n - 1);
+        reverse(arr, 0, n - 1);
+
+    }
+
+    private static void reverse(int[] arr, int x, int y) {
+        while (x <= y) {
+            int temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+            x++;
+            y--;
+        }
+    }
+
 }
