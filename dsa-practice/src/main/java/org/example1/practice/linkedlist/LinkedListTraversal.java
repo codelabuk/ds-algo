8bf252cb7da210bd747d9824783c7a91cb29be37
codelabuk <afzal232@gmail.com>
2025-06-14 09:25:34 +0530

    Replace linkedlist traversal format

--- dsa-practice/src/main/java/org/example1/practice/linkedlist/LinkedListTraversal.java
+++ dsa-practice/src/main/java/org/example1/practice/linkedlist/LinkedListTraversal.java
@@ -3,29 +3,40 @@ package org.example1.practice.linkedlist;
 public class LinkedListTraversal {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4,5};
-        
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+
         Node head = convertToLinkedList(arr);
         printNodes(head);
+        System.out.println(lengthOfLinkedList(head));
     }
 
     private static Node convertToLinkedList(int[] arr) {
         Node head = new Node(arr[0]);
         Node mover = head;
-        for(int i =1 ; i < arr.length; i++){
-            Node temp =  new Node(arr[i]);
-             mover.next = temp;
-             mover = temp;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
         }
 
-        return  head;
+        return head;
 
     }
 
-    private  static void  printNodes(Node head){
-        while(head != null){
+    private static void printNodes(Node head) {
+        while (head != null) {
             System.out.print(head.data + " ");
             head = head.next;
         }
+        System.out.println();
+    }
+
+    private static int lengthOfLinkedList(Node head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
     }
 }
